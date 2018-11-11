dbada3a56d688ad6040f22d508d561196bad4168
Grigory Polyakov <gigakun@bk.ru>
2018-11-11 18:40:27 +0300

    Sort output values by frequency of matches

--- symbstat/main.cpp
+++ symbstat/main.cpp
@@ -5,27 +5,44 @@
 #include <fstream>
 #include <memory>
 
+#include <unordered_map>
+#include <set>
+
 #include "filewrapper.h"
 #include "argsparser.h"
+
+struct valueCmp {
+    bool operator()(std::pair<char, uint64_t> a, std::pair<char, uint64_t> b) {
+        bool result = false;
+        if(a.second == b.second)
+        {
+            result = a.first < b.first;
+        }
+        else
+        {
+            result = a.second < b.second;
+        }
+        return result;
+    }
+};
 
 int main(int argc, char *argv[])
 {
     try {
         std::unique_ptr<IAppConfig> config(new ArgsParser(argc, argv));
 
-
         FileWrapper file(config->GetFileName());
         char currentSymbol;
-        std::map<char, uint64_t> symbolMatchCounter;
+        std::unordered_map<char, uint64_t> symbolMatchCounter;
         while(file.GetChar(currentSymbol))
         {
             symbolMatchCounter[currentSymbol]++;
         }
-        for(auto symbStat : symbolMatchCounter)
+        std::multiset<std::pair<char, uint64_t>, valueCmp> multisetSortedByValue(symbolMatchCounter.begin(), symbolMatchCounter.end());
+        for(auto symbStat : multisetSortedByValue)
         {
             std::cout << " Sym: " << symbStat.first << "\t\t " << symbStat.second << std::endl;
         }
-
     }
     catch (std::string e)
     {
